196ac367698779110f85b3d43c5139424b6cdc78
hieumous <hieutv0599@ut.edu.vn>
2025-11-12 19:19:10 +0700

    update

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,9 +1,1 @@
-import { LoaderIcon } from "lucide-react";
-function PageLoader() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <LoaderIcon className="size-10 animate-spin" />
-    </div>
-  );
-}
-export default PageLoader;
+*tranhung
